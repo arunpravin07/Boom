8ff91a6c3a3c3d168dbc48ea295942eb50398fd2
arunpravin07 <43900333+arunpravin07@users.noreply.github.com>
2021-05-28 19:55:38 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,8 +1,21 @@
-public class Sample {
-    public static void main(String[] args)
-    {
-        // Using Console to input data 
-        String name = System.console().readLine();
-        System.out.println("You entered string " + name);
-    }
+
+// Program to check for command line arguments
+class Sample{
+	public static void main(String[] args)
+	{
+		// check if length of args array is
+		// greater than 0
+		if (args.length > 0) {
+			System.out.println(
+				"The text from i/p is ---> ");
+
+			// iterating the args array and printing
+			// the command line arguments
+			for (String val : args)
+				System.out.println(val);
+		}
+		else
+			System.out.println("No command line "
+							+ "arguments found.");
+	}
 }
