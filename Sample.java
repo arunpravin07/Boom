0d9cdaca607f66befdc118c2458f555d9e024a17
arunpravin07 <43900333+arunpravin07@users.noreply.github.com>
2021-05-28 14:02:23 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,6 +1,7 @@
 public class Sample{
 public static void main(String[] args) {
-String name = System.console().readLine();
+  String name=null;
+ name = System.console().readLine();
   System.out.println("your o/p ---> " + name);
 }
 }
